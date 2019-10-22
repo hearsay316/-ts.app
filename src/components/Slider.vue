4d508886258a4d7d1871aee40b6f4e3b02662347
hearsay316 <hearsay316@gmail.com>
2019-10-22 18:01:53 +0800

    1 封装组件

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -54,6 +54,7 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+
 .swiper-container {
     width: 100%;
     height: 300px;
--- vue.config.js
+++ vue.config.js
@@ -5,17 +5,18 @@ module.exports = {
       // 为生产环境修改的
     } else {
       // 为开发环境修改的
-      return {
-        resolve: {
-          alias: {
-            "@config": path.resolve(__dirname, "./src/config"),
-            "@css": path.resolve(__dirname, "./src/assets/css"),
-            "@view": path.resolve(__dirname, "./src/view"),
-            "@img": path.resolve(__dirname, "./src/assets/img"),
-            "@com": path.resolve(__dirname, "./src/components")
-          }
-        }
-      };
     }
+
+    return {
+      resolve: {
+        alias: {
+          "@config": path.resolve(__dirname, "./src/config"),
+          "@css": path.resolve(__dirname, "./src/assets/css"),
+          "@view": path.resolve(__dirname, "./src/view"),
+          "@img": path.resolve(__dirname, "./src/assets/img"),
+          "@com": path.resolve(__dirname, "./src/components")
+        }
+      }
+    };
   }
 };
