bf73d0edae063f275bbb4ae2509c07559af1f582
hearsay316 <hearsay316@gmail.com>
2019-10-30 13:53:05 +0800

    1 语义化 函数名字 2 封装了activity组件

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,23 +2,37 @@
 <template>
   <div class="index">
     <Slider class="index-picData" :pices="picData"></Slider>
+    <activity>
+      <div class="activity-520">
+        <img
+          class="activity-520-img"
+          v-for="item of activityDatas"
+          :src="item.icon"
+          :alt="item.icon"
+          :key="item.id"
+        />
+      </div>
+    </activity>
   </div>
 </template>
 
 <script>
 import Slider from "@com/Slider";
-import { GetSwiper } from "../config/ajax/IndexApi.js";
+import activity from "@com/activity.vue";
+
+import { ToSwiperPic, ToActivityPic } from "../config/ajax/IndexApi.js";
 export default {
   name: "Index",
   methods: {},
   data() {
     return {
       lodings: "",
-      picData: []
+      picData: [],
+      activityDatas: []
     };
   },
   mounted() {
-    GetSwiper()
+    ToSwiperPic()
       .then(res => {
         let {
           stata,
@@ -26,10 +40,18 @@ export default {
         } = res;
         this.picData = list;
       })
-      .catch();
+      .catch(error => {});
+    ToActivityPic().then(res => {
+      let {
+        stata,
+        data: { list }
+      } = res;
+      this.activityDatas = list;
+    });
   },
   components: {
-    Slider
+    Slider,
+    activity
   }
 };
 </script>
@@ -38,6 +60,7 @@ export default {
   @import "~@css/style.styl"
 .index
     height:100%
+    background-color lineColor
 .index-picData{
   height:swiperHeigth
 }
@@ -45,4 +68,13 @@ export default {
     background: #ededed;
     width: 12px;
 }
+  .activity-520{
+    margin-top px2rem(-8)
+    border-top-left-radius px2rem(-8)
+    border-top-right-radius px2rem(-8)
+  }
+  .activity-520-img{
+      display inline-block
+      width  33.33%
+  }
 </style>
---
+++ src/components/modeOptions.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="mode-options">
+    <div class="mode-options-item"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "modeOptions",
+  data() {
+    return {
+      dataSource: [
+        {
+          id: "1",
+          icon: require("@img/jingDongCHaoShi.png"),
+          title: "京东超市"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped></style>
