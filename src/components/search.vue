fc15628fc120e233d321266da4b046533e3bcdd4
hearsay316 <hearsay316@gmail.com>
2019-11-15 23:17:49 +0800

    1 navigationBar-left-img的样式

---
+++ src/components/search.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="search">
+    <img :src="icon" alt="" class="search" />
+    <span>大前端开发混合京东商城</span>
+  </div>
+</template>
+
+<!--suppress NpmUsedModulesInstalled -->
+<script>
+export default {
+  name: "search",
+  props: {
+    icon: {
+      default: require("@img/search.svg"),
+      type: String
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+  @import "~@css/style.styl"
+
+.search{
+  background-color white
+  width 100%
+  margin px2rem(6)
+  border-radius px2rem(22)
+  display flex
+  align-items center
+}
+</style>
